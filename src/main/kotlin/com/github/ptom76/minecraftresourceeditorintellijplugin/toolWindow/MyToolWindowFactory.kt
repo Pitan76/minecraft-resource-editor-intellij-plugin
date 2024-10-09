84ac08acf3552a1b23e980b1fc23532bb24b2cf3
GitHub Action <action@github.com>
2024-10-09 03:27:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/ptom76/minecraftresourceeditorintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ptom76/minecraftresourceeditorintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ptom76.minecraftresourceeditorintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ptom76.minecraftresourceeditorintellijplugin.MyBundle
+import com.github.ptom76.minecraftresourceeditorintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
